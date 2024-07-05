433951cae0ab3ed57d74eb89aff45693b553d2c4
Lembo <54678184+lembo2003@users.noreply.github.com>
2024-07-06 03:34:08 +0700

    Finished detailcss
    Done with some frontend side

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './chat.css'
 import EmojiPicker from 'emoji-picker-react'
 const Chat = () => {
   const [open,setOpen] = useState(false);
   const [text,setText] = useState("");
+
+  const endRef = useRef(null)
+ 
+  useEffect(()=>{
+    endRef.current?.scrollIntoView({behavior:"smooth"});
+  },[])
+
   const handleEmoji = e => {
     setText(prev=>prev+e.emoji);
     setOpen(false);
@@ -56,6 +63,7 @@ const Chat = () => {
           <span>1 min ago</span>
           </div>
         </div>
+        <div ref={endRef}></div>
       </div>
       
       <div className="bottom">
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -2,7 +2,56 @@ import './detail.css'
 
 const Detail = () => {
   return (
-    <div className='detail'>Detail</div>
+    <div className='detail'>
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>To beo</h2>
+        <p>Lorem ipsum dolor sit amet, </p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>  <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>  <div className="option">
+          <div className="title">
+            <span>Shared photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+            <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT62iPsf4eg1YHuGNzeOEE1e5v-WFiQ1kPK4fcN0D1h4kFtSth-" alt="" />
+            <span>photo_2024.png</span>
+              </div>
+            <img src="./download.png" alt=""  className="icon" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+            <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT62iPsf4eg1YHuGNzeOEE1e5v-WFiQ1kPK4fcN0D1h4kFtSth-" alt="" />
+            <span>photo_2024.png</span>
+              </div>
+            <img src="./download.png" alt=""  className="icon" />
+            </div>
+
+           
+          </div>
+        </div>  <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className='logout'>Logout</button>
+      </div>
+    </div>
   )
 }
 
