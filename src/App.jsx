97c27858fd914111bbfc2f0b38086c8e8499f3c0
Lembo <54678184+lembo2003@users.noreply.github.com>
2024-07-08 17:30:11 +0700

    fixed ? i guess
    Dont know what happened but currently there's a bug when you dont put in avatar, it just straightup doesnt work anymore

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
   useEffect(()=>{
     const unSub = onAuthStateChanged(auth,(user)=>{
       fetchUserInfo(user?.uid);
-
+      console.log(user)
     })
 
     return ()=>{
--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -1,19 +1,46 @@
+
+import { collection, getDoc, query, where } from 'firebase/firestore'
 import './addUser.css'
+import { db } from '../../../../lib/firebase'
+import { useState } from 'react'
 
 const AddUser = () => {
+  const [user,setUser] = useState(null)
+  const handleSearch = async(e) =>{
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const username = formData.get("username")
+
+    try {
+
+      const userRef = collection(db, "users");
+
+    const q = query(userRef, where("username", "==", username));
+    const querySnapshot = await getDoc(q)
+
+    if(!querySnapshot.empty){
+      setUser(querySnapshot.docs[0].data())
+    }
+
+    }catch(err){
+      console.log(err)
+    }
+  }
+
   return (
     <div className='addUser'>
-        <form action="">
+        <form action="" onSubmit={handleSearch}>
             <input type="text" placeholder='Username' name="username" />
             <button>Search</button>
         </form>
-        <div className="user">
+        
+{   user &&     <div className="user">
             <div className="detail">
-                <img src="./avatar.png" alt="" />
-                <span>To beo</span>
+                <img src={user.avatar || "./avatar.png"} alt="" />
+                <span>{user.username}</span>
             </div>
             <button>Add user</button>
-        </div>
+        </div>}
     </div>
   )
 }
