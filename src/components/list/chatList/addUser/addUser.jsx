02f7630c8ebdc19173d73411684a46e86bdd09f7
Lembo <54678184+lembo2003@users.noreply.github.com>
2024-07-11 01:26:56 +0700

    problem, userchats collection missing

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -1,11 +1,14 @@
 
-import { collection, getDoc, getDocs, query, where } from 'firebase/firestore'
+import { arrayUnion, collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore'
 import './addUser.css'
 import { db } from '../../../../lib/firebase'
 import { useState } from 'react'
+import { useUserStore } from '../../../../lib/userStore'
 
 const AddUser = () => {
   const [user,setUser] = useState(null)
+
+  const {currentUser} = useUserStore()
   const handleSearch = async (e) =>{
     e.preventDefault()
     const formData = new FormData(e.target)
@@ -27,6 +30,46 @@ const AddUser = () => {
     }
   }
 
+  const handleAdd = async ()=>{
+
+    const chatRef = collection(db,"chats",)
+    const userChatRef = collection(db,"userchats",)
+    try{
+      const newChatRef = doc(chatRef)
+      
+      await setDoc(newChatRef,
+        {
+          createdAt: serverTimestamp(),
+        messages: [],
+      })
+
+      await updateDoc(doc(userChatRef, user.id),{
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage:"",
+          receiverId: currentUser.id,
+          updatedAt: Date.now(),
+        })
+      });
+
+      await updateDoc(doc(userChatRef, currentUser.id),{
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage:"",
+          receiverId: user.id,
+          updatedAt: Date.now(),
+        })
+      });
+
+      
+    }
+
+  catch(err){
+    console.log(err);
+  }
+
+  }
+
   return (
     <div className='addUser'>
         <form action="" onSubmit={handleSearch}>
@@ -38,7 +81,7 @@ const AddUser = () => {
                 <img src={user.avatar || "./avatar.png"} alt="" />
                 <span>{user.username}</span>
             </div>
-            <button>Add user</button>
+            <button onClick={handleAdd}>Add user</button>
         </div>}
     </div>
       
